0e5b6cdc568d613b494e59db25cd4ae265430b8c
farhankhan8800 <38123762+farhankhan8800@users.noreply.github.com>
2022-11-07 15:13:00 +0530

    @farhan commit

--- src/pages/Deals/DealDetail.js
+++ src/pages/Deals/DealDetail.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom';
 
 
 import Deals from './ProductDetail'
 import SimilarMoreProducts from '../../component/SimilarMoreProducts'
 const DealDetails = () => {
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get('utm_source')); // ▶ URLSearchParams {}
+  console.log(searchParams.get('name')); // ▶ URLSearchParams {}
+
   return (
     <>
       <div style={{paddingTop:"56px"}} classname="hot-deal-con" id="deals">
